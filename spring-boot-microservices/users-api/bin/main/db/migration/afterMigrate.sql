8c9fe16f0ab5aa1041727b8eb9f1b877a6800164
mohitchhapru <chhaprumohit@gmail.com>
2018-05-21 14:32:43 -0700

    Added Angular Boiler plate

--- spring-boot-microservices/users-api/bin/main/db/migration/afterMigrate.sql
+++ spring-boot-microservices/users-api/bin/main/db/migration/afterMigrate.sql
@@ -2,4 +2,10 @@ INSERT INTO USERS
 	(USER_NAME, FIRST_NAME, LAST_NAME)
 VALUES
 	('chhaprumohit','Mohit','Chhapru'),
-	('mendesdion','Dion','Mendes')
+	('mendesdion','Dion','Mendes');
+
+
+INSERT INTO EVENTS
+    (TITLE, CATEGORY, DESCRIPTION, EVENT_DATE, ADDRESS, CONTACT_NAME, CONTACT_PHONE, CONTACT_EMAIL)
+values
+    ('ABC Event', 'Cat 1', 'Description of the Event', '2018-10-19 10:23:54+02', 'Address of the event', 'Mohit', '3132216099', 'chhaprumohit@gmail.com');
--- spring-boot-microservices/users-api/src/main/resources/db/migration/afterMigrate.sql
+++ spring-boot-microservices/users-api/src/main/resources/db/migration/afterMigrate.sql
@@ -3,9 +3,3 @@ INSERT INTO USERS
 VALUES
 	('chhaprumohit','Mohit','Chhapru'),
 	('mendesdion','Dion','Mendes');
-
-
-INSERT INTO EVENTS
-    (TITLE, CATEGORY, DESCRIPTION, EVENT_DATE, ADDRESS, CONTACT_NAME, CONTACT_PHONE, CONTACT_EMAIL)
-values
-    ('ABC Event', 'Cat 1', 'Description of the Event', '2018-10-19 10:23:54+02', 'Address of the event', 'Mohit', '3132216099', 'chhaprumohit@gmail.com');
