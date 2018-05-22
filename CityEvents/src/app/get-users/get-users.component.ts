2f28cc2b383440baa1007b098286e9dfe01b7615
mohitchhapru <chhaprumohit@gmail.com>
2018-05-22 10:56:02 -0700

    Implemented auto reload by merging code for add user is users component

--- CityEvents/src/app/get-users/get-users.component.ts
+++ CityEvents/src/app/get-users/get-users.component.ts
@@ -10,11 +10,25 @@ import {UsersService} from '../users.service';
 
 export class GetUsersComponent implements OnInit {  
   users:any;
+  private userName;
+  private firstName;
+  private lastName;
+
   constructor(private userService:UsersService) { }
 
   ngOnInit() {
     this.userService.allUsers()    
     .subscribe(res => this.users=res.json());    
   }
+
+  addNewUser(){
+    console.log("Adding : "+this.userName+this.firstName+this.lastName);    
+    this.userService.addUser(this.userName, this.firstName,this.lastName).subscribe(res=>{
+      this.users.push(res.json());
+      this.userName="";
+      this.firstName="";
+      this.lastName="";
+    })
+  }    
 
 }
